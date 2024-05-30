41b0cb51ccd54969eaad6f8ea11b1a2107b08b28
Timothy Warner <timothy.warner15@gmail.com>
2024-05-30 11:40:05 -0400

    v0.1.1 Updated version improving code efficiency and debugging

--- flux_calculator.py
+++ flux_calculator.py
@@ -1,96 +1,56 @@
 import numpy as np
 import pandas as pd
 
-# read csv files
-df_Channel_0_data = pd.read_csv('data/Channel_0.csv')
-df_Channel_1_data = pd.read_csv('data/Channel_1.csv')
-df_Channel_2_data = pd.read_csv('data/Channel_2.csv')
+# Read csv files
+channels = [pd.read_csv(f'data/Channel_{i}.csv') for i in range(3)]
 df_constants = pd.read_csv('constants.csv')
 df_test_conditions = pd.read_csv('test_conditions.csv')
 
-# create np.arrays with mass data, constants and test conditions
-mass_0_data = pd.DataFrame(df_Channel_0_data['Weight [Bridge Input Ch:0 -> 1046 S/N:583686]']).to_numpy()
-mass_1_data = pd.DataFrame(df_Channel_1_data['Weight [Bridge Input Ch:1 -> 1046 S/N:583686]']).to_numpy()
-mass_2_data = pd.DataFrame(df_Channel_2_data['Weight [Bridge Input Ch:2 -> 1046 S/N:583686]']).to_numpy()
-constants = pd.DataFrame(df_constants).to_numpy()
-test_time = pd.Series(df_test_conditions['Measurement Start time (24hr time)']).to_numpy()
-test_pressure = pd.Series(df_test_conditions['Pressure (PSI)']).to_numpy()
+# Extract relevant data from the channels and convert to numpy arrays
+mass_data = [channel[f'Weight [Bridge Input Ch:{i} -> 1046 S/N:583686]'].to_numpy() for i, channel in
+             enumerate(channels)]
+constants = df_constants.to_numpy()
+test_time = df_test_conditions['Measurement Start time (24hr time)'].to_numpy()
+test_pressure = df_test_conditions['Pressure (PSI)'].to_numpy()
 
-# allocate variables associated with 'constants.csv'
-length = constants[0, 0]  # length in cm
-diameter = constants[0, 1]  # diameter in mm
-fiber_number = constants[0, 2]  # integer
-duration = constants[0, 3]  # time in seconds
+# Allocate variables associated with 'constants.csv'
+length, diameter, fiber_number, duration = constants[0]
 
-# calculate the hollow fiber surface area
-surface_area = 2 * np.pi * diameter / 2 / 1000 * length / 100 * fiber_number
+# Calculate the hollow fiber surface area
+surface_area = np.pi * diameter / 1000 * length / 100 * fiber_number
 
-# convert pressure from PSI to bar
-test_pressure_bar = test_pressure*0.0689476
+# Convert pressure from PSI to bar
+test_pressure_bar = test_pressure * 0.0689476
 
-# create rolling average vectors
-rolling_average_0 = np.zeros_like(mass_0_data, dtype=float)
-rolling_average_1 = np.zeros_like(mass_1_data, dtype=float)
-rolling_average_2 = np.zeros_like(mass_2_data, dtype=float)
-
-# window (seconds) over which the data is smoothed (averaged)
+# Create rolling average vectors
 window_size = 10
+rolling_averages = [pd.Series(mass).rolling(window=window_size, center=True).mean().to_numpy() for mass in mass_data]
 
-# fill array with averaged values - must be done for all three channels due to the fact they could be different sizes
-for i in range(window_size // 2, len(mass_0_data) - window_size // 2):
-    rolling_average_0[i] = np.mean(mass_0_data[i - window_size // 2 + 1: i + window_size // 2 + 1])
-
-for i in range(window_size // 2, len(mass_1_data) - window_size // 2):
-    rolling_average_1[i] = np.mean(mass_1_data[i - window_size // 2 + 1: i + window_size // 2 + 1])
-
-for i in range(window_size // 2, len(mass_2_data) - window_size // 2):
-    rolling_average_2[i] = np.mean(mass_2_data[i - window_size // 2 + 1: i + window_size // 2 + 1])
-
-# preallocate flux vectors
-flux_0 = np.zeros(len(test_time))
-flux_1 = np.zeros(len(test_time))
-flux_2 = np.zeros(len(test_time))
+# Preallocate flux vectors
+flux = np.zeros((3, len(test_time)))
 
 # Calculate the flux values for each of the channels at all the test times specified in test_conditions.csv
-for i in range(0, len(test_time)):
-    # Define the substring to match the specific test time for each loop
-    substring = df_test_conditions['Measurement Start time (24hr time)'].iloc[i]
+for i, start_time in enumerate(test_time):
+    for j in range(3):
+        matching_rows = channels[j][channels[j]['Date'].str.contains(start_time)]
+        if matching_rows.empty:
+            print(f'Error - Data missing for Channel_{j} at time {start_time}')
+        if not matching_rows.empty:
+            time_index = matching_rows.index[0]
+            mass_change = (rolling_averages[j][time_index + duration.astype(int) - 1] -
+                           rolling_averages[j][time_index]) / 1000
+            flux[j, i] = mass_change / surface_area / (duration / 3600)
 
-    # Find rows where the 'Date' (time) column contains the substring
-    matching_row_0 = df_Channel_0_data[df_Channel_0_data['Date'].str.contains(substring)]
-    matching_row_1 = df_Channel_1_data[df_Channel_1_data['Date'].str.contains(substring)]
-    matching_row_2 = df_Channel_2_data[df_Channel_2_data['Date'].str.contains(substring)]
-
-    # Get the indices of the matching rows
-    time_index_0 = matching_row_0.index
-    time_index_1 = matching_row_1.index
-    time_index_2 = matching_row_2.index
-
-    # calculate the change in mass (kg) over Test Duration time specified in 'constants.csv' by calculating
-    # ( mass(final) - mass(initial) ) / 1000 which includes unit conversion from g to kg.
-    mass_change_0 = (rolling_average_0[time_index_0 + duration.astype(int) - 1] - rolling_average_0[time_index_0])/1000
-    mass_change_0 = mass_change_0.item()
-    mass_change_1 = (rolling_average_1[time_index_1 + duration.astype(int) - 1] - rolling_average_1[time_index_1])/1000
-    mass_change_1 = mass_change_1.item()
-    mass_change_2 = (rolling_average_2[time_index_2 + duration.astype(int) - 1] - rolling_average_2[time_index_2])/1000
-    mass_change_2 = mass_change_2.item()
-
-    # calculate the flux converting seconds to hours
-    flux_0[i] = mass_change_0 / surface_area / (duration / 3600)
-    flux_1[i] = mass_change_1 / surface_area / (duration / 3600)
-    flux_2[i] = mass_change_2 / surface_area / (duration / 3600)
 
 # Create data series of output variables to be exported to a .csv file
-output_time = df_test_conditions['Measurement Start time (24hr time)']
-output_pressure_bar = pd.Series(test_pressure_bar, name='Pressure (bar)')
-output_flux_0 = pd.Series(flux_0, name='Flux 0 (LMH)')
-output_flux_1 = pd.Series(flux_1, name='Flux 1 (LMH)')
-output_flux_2 = pd.Series(flux_2, name='Flux 2 (LMH)')
+output_data = {
+    'Measurement Start time (24hr time)': df_test_conditions['Measurement Start time (24hr time)'],
+    'Pressure (bar)': test_pressure_bar,
+    'Flux 0 (LMH)': flux[0],
+    'Flux 1 (LMH)': flux[1],
+    'Flux 2 (LMH)': flux[2],
+}
 
-# Concatenate output data series into output data frame
-df_outputs = pd.concat([output_time, output_pressure_bar, output_flux_0, output_flux_1, output_flux_2], axis=1)
-
-# Export outputs to .csv file
-df_outputs.to_csv(f"outputs.csv")
-
-print(df_outputs)
+# Create output DataFrame and export to .csv file
+df_outputs = pd.DataFrame(output_data)
+df_outputs.to_csv("outputs.csv", index=False)
